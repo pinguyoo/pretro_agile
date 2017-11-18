8f89c9bf0bf5bf680368ea130953b3c0eccee9f2
Jacklee <jack19820814@gmail.com>
2017-11-18 14:16:45 +0800

    show the comment from controller

--- resources/views/new-comment.blade.php
+++ resources/views/new-comment.blade.php
@@ -1,6 +1,36 @@
 @extends('app')
 @section('content')
 
+@if (count($data))
+
+  <div class="col">
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Type</th>
+            <th scope="col">Content</th>
+            <th scope="col">created_at</th>
+            </tr>
+        </thead>
+        <tbody>
+          @foreach ($data as $info)
+          <tr>
+            <th scope="row">{{ $info->id }}</th>
+            <td>{{ $info->type}}</td>
+            <td>{{ $info->content}}</td>
+            <td>{{ $info->created_at}}</td>
+          </tr>
+          @endforeach
+
+        </tbody>
+    </table>
+  </div>
+
+@endif
+
+  {{ $data }}
+
   <div class="col">
     <form class="" action="/new-comment" method="POST">
       {{ csrf_field() }}
