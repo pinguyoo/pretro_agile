25061045a6b02b881527a7d1a5677fa1fa79e17b
Jacklee <jack19820814@gmail.com>
2017-11-24 17:47:20 +0800

    change comment css

--- resources/views/new-comment.blade.php
+++ resources/views/new-comment.blade.php
@@ -1,9 +1,31 @@
 @extends('app')
+
 @section('content')
 
 @if (count($comment))
 
-  <div class="col">
+<div class="row row-eq-height">
+
+  @foreach ($comment as $key=>$info)
+    <div class="col-3 m-0 t-0">
+      <div class="card" style="width: 20rem;">
+        <div class="card-body">
+          <h4 class="card-title">#{{$key+1}}</h4>
+          <h6 class="card-subtitle mb-2 text-muted">{{$info->created_at}}</h6>
+          <p class="card-text">{{$info->content}}</p>
+          <form class="" action="/new-comment/{{ $info->id }}" method="post">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <Button type="submit" name="remove" class="btn btn-danger">Remove</Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  @endforeach
+
+</div>
+
+  <!-- <div class="col">
     <table class="table">
         <thead>
             <tr>
@@ -19,7 +41,7 @@
           <tr>
             <th scope="row">{{ $key+1 }}</th>
             <td>{{ $info->type}}</td>
-            <td>{{ $info->content}}</td>
+            <td contenteditable='true'>{{ $info->content}}</td>
             <td>{{ $info->created_at}}</td>
             <td>
               <form class="" action="/new-comment/{{ $info->id }}" method="post">
@@ -33,7 +55,7 @@
 
         </tbody>
     </table>
-  </div>
+  </div> -->
 
 @endif
 
